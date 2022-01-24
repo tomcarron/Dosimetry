b9681d03b632947c45dd85fbc03d2f379b74f963
tomcarron <tcarron@tcd.ie>
2022-01-24 19:10:52 +0100

    added plots

--- dosimetry.py
+++ dosimetry.py
@@ -22,10 +22,33 @@ print(shielding_Pb)
 '''
 distance=(distance_measurements.to_numpy()[:,0]).astype(np.float64)
 Cs_x_counts=(distance_measurements.to_numpy()[:,1]).astype(np.float64)
-#print(distance)
+Co_x_counts=(distance_measurements.to_numpy()[:,2]).astype(np.float64)
+Cs2_x_counts=(distance_measurements.to_numpy()[:,3]).astype(np.float64)
+Na_x_counts=(distance_measurements.to_numpy()[:,4]).astype(np.float64)
 
 plt.figure(0)
 plt.scatter(distance,Cs_x_counts)
-#plt.xlim(0,20)
-#plt.ylim(0,1000)
+plt.xlabel('distance (cm)')
+plt.ylabel('Counts')
+plt.savefig('plots/Cs_dist.png',dpi=400,bbox_inches='tight')
+
+plt.figure(1)
+plt.scatter(distance,Co_x_counts)
+plt.xlabel('distance (cm)')
+plt.ylabel('Counts')
+plt.savefig('plots/Co_dist.png',dpi=400,bbox_inches='tight')
+
+plt.figure(2)
+plt.scatter(distance,Cs2_x_counts)
+plt.xlabel('distance (cm)')
+plt.ylabel('Counts')
+plt.savefig('plots/Cs2_dist.png',dpi=400,bbox_inches='tight')
+
+plt.figure(3)
+plt.scatter(distance,Na_x_counts)
+plt.xlabel('distance (cm)')
+plt.ylabel('Counts')
+plt.savefig('plots/Na_dist.png',dpi=400,bbox_inches='tight')
+
+
 plt.show()
