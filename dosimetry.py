8340bc30067d2f09179da56fc86005ee363add90
tomcarron <tcarron@tcd.ie>
2022-01-24 20:05:05 +0100

    Subtracted background from activities in distance measurements

--- dosimetry.py
+++ dosimetry.py
@@ -24,9 +24,13 @@ Distance measurements to arrays. counts measured for 60s
 '''
 distance=(distance_measurements.to_numpy()[:,0]).astype(np.float64)
 Cs_x_counts=(distance_measurements.to_numpy()[:,1]).astype(np.float64)
+Cs_x_N=Cs_x_counts-np.full(len(Cs_x_counts),background_per_minute)   #Background subtracted
 Co_x_counts=(distance_measurements.to_numpy()[:,2]).astype(np.float64)
+Co_x_N=Co_x_counts-np.full(len(Co_x_counts),background_per_minute)   #Background subtracted
 Cs2_x_counts=(distance_measurements.to_numpy()[:,3]).astype(np.float64)
+Cs2_x_N=Cs2_x_counts-np.full(len(Cs2_x_counts),background_per_minute)   #Background subtracted
 Na_x_counts=(distance_measurements.to_numpy()[:,4]).astype(np.float64)
+Na_x_N=Na_x_counts-np.full(len(Na_x_counts),background_per_minute)  #Background subtracted
 
 '''
 shielding measurements to array
@@ -47,25 +51,25 @@ neutron_dt=(neutron_activity.to_numpy()[:,2]).astype(np.float64)
 Plotting distance measurements
 '''
 plt.figure(0)
-plt.scatter(distance,Cs_x_counts)
+plt.scatter(distance,Cs_x_N)
 plt.xlabel('distance (cm)')
 plt.ylabel('Counts')
 plt.savefig('plots/Cs_dist.png',dpi=400,bbox_inches='tight')
 
 plt.figure(1)
-plt.scatter(distance,Co_x_counts)
+plt.scatter(distance,Co_x_N)
 plt.xlabel('distance (cm)')
 plt.ylabel('Counts')
 plt.savefig('plots/Co_dist.png',dpi=400,bbox_inches='tight')
 
 plt.figure(2)
-plt.scatter(distance,Cs2_x_counts)
+plt.scatter(distance,Cs2_x_N)
 plt.xlabel('distance (cm)')
 plt.ylabel('Counts')
 plt.savefig('plots/Cs2_dist.png',dpi=400,bbox_inches='tight')
 
 plt.figure(3)
-plt.scatter(distance,Na_x_counts)
+plt.scatter(distance,Na_x_N)
 plt.xlabel('distance (cm)')
 plt.ylabel('Counts')
 plt.savefig('plots/Na_dist.png',dpi=400,bbox_inches='tight')
